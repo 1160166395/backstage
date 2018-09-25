c9ab4f7113dae17d39fe943987ca99602430f19b
winter <1160166395@qq.com>
2018-09-25 14:23:14 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+Vue.use(ElementUI);
+import 'element-ui/lib/theme-chalk/index.css';
+import axios from 'axios';
+Vue.use(axios);
+
+Vue.prototype.$axios = axios;
+//import '@/sass/base.scss';
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  //margin-top: 60px;
+}
+</style>
---
+++ src/components/home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="home">
+        <mt-search v-model="keyword"></mt-search>
+        <router-view><router-view/>
+    </div>
+</template>
+<script>
+
+    export default {
+        data(){
+            return {
+                items:[],
+                keyword:''
+            }
+        },
+        created(){
+            
+        }
+    }
+</script>
+<style scoped>
+    
+</style>
